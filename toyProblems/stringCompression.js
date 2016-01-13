ad6014aea025ccde65034a97af70c2d87edbaa83
Anna Rogers <2muddybarefeet@gmail.com>
2016-01-13 10:42:25 -0800

    stringCompression Solution

---
+++ toyProblems/stringCompression.js
@@ -0,0 +1,33 @@
+//take a string of characters and compress it to a string of letters and their frequencies
+//if the original string is not longer than the compressed string return the original String
+//the string only contains letters
+
+//example = "aaabbbccccc" ----> "a3b3c5"
+//another example = "aabbcc" -----> "aabbcc"
+
+var stringCompression = function (str) {
+
+  var letterFreq = {};
+  var result = "";
+
+  //make an object of letter frequencies and make string from this
+  for (var i = 0; i < str.length; i++) {
+    if (letterFreq[str[i]]) {
+      letterFreq[str[i]] ++;
+    } else {
+      letterFreq[str[i]] = 1;
+    }
+  }
+
+  for (var key in letterFreq) {
+    result += key + letterFreq[key];
+  }
+  console.log(result.length, str.length);
+  //check lengths and return letter needed
+  return str.length <= result.length ? str : result;
+
+
+};
+
+// var a = stringCompression('wwwwwww');
+// console.log(a);
