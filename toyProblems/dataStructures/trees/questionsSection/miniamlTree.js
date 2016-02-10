db073d89cb75042a9b073fe0ca57d2dae2ea1de6
Anna Rogers <2muddybarefeet@gmail.com>
2016-02-10 16:05:43 +0000

    Working on solution

---
+++ toyProblems/dataStructures/trees/questionsSection/miniamlTree.js
@@ -0,0 +1,100 @@
+//given a binary tree design an algorithm which creates a linked list of all the nodes at each depth
+
+//make a binary tree diagram constructor
+
+var Tree = function(number) {
+
+  var result = {};
+
+  result.value = number;
+  result.rightSide = null;
+  result.leftSide = null;
+
+  result.insert = function(num) {//insert function
+
+    if(num>this.value) {
+      if(this.rightSide === null) {
+        this.rightSide = Tree(num);
+      } else {
+        this.rightSide.insert(num);
+      }
+    } else {
+      if(this.leftSide === null) {
+        this.leftSide = Tree(num);
+      } else {
+        this.leftSide.insert(num);
+      }
+    }
+  };
+
+  return result;
+
+};
+
+var findMidAndInsert = function (tree, array) {
+//take array and get mid
+  var mid = Math.ceil(array.length/2);
+  if (array.length === 1) {
+    mid = 0;
+  }
+//insert middle num to the tree
+  tree.insert(array[mid]);
+//return two arrays with middle num removed
+  if (array.length > 1) {
+    return [array.slice(0,mid), array.slice(mid+1)];
+  }
+  return false;
+};
+
+
+var makeTree = function (sortedArray) {
+
+    //find the midpoint (floor)
+    var midpoint = Math.floor(sortedArray.length/2);
+    var tree = new Tree(sortedArray[midpoint]);
+
+    var left = sortedArray.slice(0,midpoint);
+    var right = sortedArray.slice(midpoint+1);
+
+    var inner = function (left, right) {
+
+      if(left) { //on left side get two extra empty leaves??!!
+
+        //take mid index of the array
+        var mid = Math.ceil(left.length/2);
+        if (left.length === 1) {
+          mid = 0;
+        }
+        //insert it into the tree
+        tree.insert(left[mid]);
+
+        //slice
+        if (left.length > 1) {
+          inner(left.slice(0,mid),left.slice(mid+1));
+        }
+        // var newArrays = findMidAndInsert(tree, left);
+        // if (newArrays) {
+        //   inner(newArrays[0],newArrays[1]);
+        // }
+      } if (right) {
+
+        var subArrays = findMidAndInsert(tree, right);
+        if (subArrays) {
+          inner(subArrays[0], subArrays[1]);
+        }
+      }
+
+      return;
+
+    };
+
+    inner(left, right);
+    console.log('end',tree);
+    // return tree;
+
+  };
+
+
+makeTree([1,2,3,4,5,6,7,8,9]);
+
+
