fe306863116091277a75e4a06f3b801e3583dc81
Anna Rogers <2muddybarefeet@gmail.com>
2016-02-13 10:54:48 +0000

    memoize

---
+++ toyProblems/recursionAndDynamicProgramming/memoize.js
@@ -0,0 +1,29 @@
+/* Memoization: attempts to increase a function’s performance 
+by caching its previously computed results. 
+Each time a memoized function is called, its parameters are used to index the cache. If the data 
+is present, then it can be returned, without executing the entire function.  However, if the 
+data is not cached, then the function is executed, and the result is added to the cache.*/
+
+var numb = function (num) {
+  return num+29;
+};
+
+var memoize = function (fn) {
+
+  var cache = {};//function args index the cache
+
+  return function () {
+    var args = Array.prototype.slice.call(arguments);
+    // console.log('args',args.toString());
+    if (cache.hasOwnProperty(args.toString())) {
+      return cache[args];
+    }
+    cache[args] = fn(args[0]);
+    return cache[args];
+
+  };
+
+};
+
+var a = memoize(numb);
+console.log(a(5));
