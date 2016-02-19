089c1c2e4682ef325541d52e42fe8c338ebca7ab
Anna Rogers <2muddybarefeet@gmail.com>
2016-02-19 22:37:57 +0000

    memoize solution

--- toyProblems/recursionAndDynamicProgramming/memoize.js
+++ toyProblems/recursionAndDynamicProgramming/memoize.js
@@ -4,9 +4,9 @@ Each time a memoized function is called, its parameters are used to index the ca
 is present, then it can be returned, without executing the entire function.  However, if the 
 data is not cached, then the function is executed, and the result is added to the cache.*/
 
-var numb = function (num) {
-  return num+29;
-};
+// var numb = function (num) {
+//   return num+29;
+// };
 
 var memoize = function (fn) {
 
@@ -25,5 +25,5 @@ var memoize = function (fn) {
 
 };
 
-var a = memoize(numb);
-console.log(a(5));
+// var a = memoize(numb);
+// console.log(a(5));
