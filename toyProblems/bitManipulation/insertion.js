0383774623498c33df46f466a0e908d6a6a0ec97
Anna Rogers <2muddybarefeet@gmail.com>
2016-02-19 20:56:20 +0000

    Add new folder

---
+++ toyProblems/bitManipulation/insertion.js
@@ -0,0 +1,8 @@
+//you are given two 32 bit numbers and two indexes such that they are the length of the second input
+//input the second into the first at the given indexes
+
+//example:
+//N = 10000000000 and M = 1011, i = 2, j = 6
+//Output = 10001001100
+
+
