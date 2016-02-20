e8aeea0124b0188cd0894c6c731b9fb0f0b41cfd
Anna Rogers <2muddybarefeet@gmail.com>
2016-02-20 02:13:23 +0000

    Add file for flip bit to win question

---
+++ toyProblems/bitManipulation/flipBitToWin.js
@@ -0,0 +1,8 @@
+//You have a binary number and you can flip exactly one 0 to be a 1
+//write code to find the longest sequence of ones
+
+var flipBitToGetLongestOnes = function (sequence) {
+
+  //return count
+
+};
