0fcd6b6b6da28cc24ad4e4d6701e938a97bfef9b
Anna Rogers <2muddybarefeet@gmail.com>
2016-02-21 12:30:12 -0800

    Work on callCentre basic passing up functionality

--- toyProblems/objectOrientedDesign/callCentre.js
+++ toyProblems/objectOrientedDesign/callCentre.js
@@ -13,147 +13,159 @@ var Call = function (level, length) {
 };
 
 var dispatchCall = function (level, length) {
-  var call = new Call();
+  var call = new Call(level, length);
   queue.push(call);
+  console.log('queue', queue);
 };
 
 //sent by an employee to take a new call
 var findNewCall = function () {
+
   var newCall;
   if (!this.isBusy && queue.length > 0) {
-    if (this.type === 'manager') {
-      newCall = managersQueue.shift();
-    } else if (this.type === 'respondent') {
-      newCall = queue.shift();
-    } else {
-      newCall = directorsQueue.shift();
-    }
-    this.takeCall(newCall);//pass in shifted off call
+    this.takeCall(queue.shift());
+
+    //CODE FOR MANAGERS AND DIRECTORS QUEUES TO BE SHIFTED FROM ------- to be re implemented
+    
+    // if (this.type === 'manager') {
+    //   newCall = managersQueue.shift();
+    // } else if (this.type === 'respondent') {
+    // } else {
+    //   newCall = directorsQueue.shift();
+    // }
+    // console.log('for respondent', queue.shift());
+    // console.log('finding call newCall', newCall);
+    // this.takeCall(newCall);//pass in shifted off call
   }
 };
 
 
 //employee classes
-var Respondent = function () {
+var Respondent = function (managers) {
   this.type = "repondent";
   this.isBusy = false;
-  this.managers = [];
+  this.managers = managers;
   this.passedUp = false;
-  //function to check if calls ready to be answered if not busy
-  // this.checkStatus = function () { //HOW TO CALL THIS??
-  //   var that = this;
-  //   setInterval(findNewCall, 1000);
-  // };
-
-  setInterval(findNewCall.bind(this), 1000);
+  setInterval(findNewCall.bind(this), 3000);
   //function to set the isBusy for a certain period of time
-  this.takeCall = function (call) {
-    var that = this;
-    //if the call can be answered by the resondent then set isBusy
-    if (call.level === 3) {
-      setTimeout(function () {
-        that.isBusy = true;
-      }, call.length);
-    } else {
-      //loop through the managers of the respondent and find the next free one
-      for (var i = 0; i < this.managers.length; i++) {
-        if (!this.managers[i].isBusy) {
-          this.managers[i].takeCall();
-          that.passedUp = true;
-          break;
-        }
-      }
-      //if it was not passed up to a manager then add to managers queue
-      if (!that.passedUp) {
-        managersQueue.push(call);
-      }
-      that.passedUp = false;
-    }
-  };
 };
 
-var Manager = function () {
+Respondent.prototype.takeCall = function (call) {
+  var that = this;
+  console.log('take call respondent', call);
+  //if the call can be answered by the resondent then set isBusy
+  if (call.level === 3) {
+    console.log('call level should be 3', call.level);
+    this.isBusy = true;
+    setTimeout(function () {
+      that.isBusy = false;
+    }, call.length);
+    console.log('isBusy', this.isBusy);
+  } else {
+    //loop through the managers of the respondent and find the next free one
+    for (var i = 0; i < this.managers.length; i++) {
+      console.log('searching managers to see who is free');
+      if (!this.managers[i].isBusy) {
+        console.log('found not busy manager', this.managers[i]);
+        this.managers[i].takeCall(call);
+        this.passedUp = true;
+        break;
+      }
+    }
+    //if it was not passed up to a manager then add to managers queue
+    if (!this.passedUp) {
+      console.log('not found a free manager so call added to the managers queue');
+      managersQueue.push(call);
+    }
+    this.passedUp = false;
+  }
+};
+
+var Manager = function (directors) {
   this.type = "manager";
   this.isBusy = false;
-  this.directors = [];
+  this.directors = directors;
   this.passedUp = false;
   //function to check if calls ready to be answered if not busy
-  this.checkStatus = function () {
-    var that = this;
-    setInterval(findNewCall, 1000);
-  };
-  this.takeCall = function (call) {
-    var that = this;
-    if (call.level === 2) {
-      setTimeout(function () {
-        that.isBusy = true;
-      }, call.length);
-    } else {
-      //if not the right level then needs passing to director
-      for (var i = 0; i < this.directors.length; i++) {
-        if (!this.directors[i].isBusy) {
-          this.directors[i].takeCall();
-          this.passedUp = true;
-          break;
-        }
-      }
-      if (!this.passedUp) {
-        //need to add to directors queue
-        directorsQueue.push(call);
-      }
-      this.passedUp = false;
-    }
-  };
+  // setInterval(findNewCall.bind(this), 3000);
+};
 
+Manager.prototype.takeCall = function (call) {
+  console.log('take call manager', call);
+  var that = this;
+  if (call.level === 2) {
+    this.isBusy = true;
+    setTimeout(function () {
+      that.isBusy = false;
+    }, call.length);
+    console.log('busy', this.isBusy);
+  } else {
+    debugger;
+    //if not the right level then needs passing to director
+    for (var i = 0; i < this.directors.length; i++) {
+      console.log('looking for free director');
+      if (!this.directors[i].isBusy) {
+        console.log('found free director', this.directors[i]);
+        this.directors[i].takeCall(call);
+        this.passedUp = true;
+        break;
+      }
+    }
+    if (!this.passedUp) {
+      //need to add to directors queue
+      directorsQueue.push(call);
+    }
+    this.passedUp = false;
+  }
 };
 
 var Director = function () {
   this.type = "director";
   this.isBusy = false;
   //look in the directors queue and see if any calls to answer
-  this.checkStatus = function () {
-    var that = this;
-    setInterval(findNewCall, 1000);
-  };
-  //take a call
-  this.takeCall = function (call) {
-    var that = this;
-    if (call.level === 2) {
-      setTimeout(function () {
-        that.isBusy = true;
-      }, call.length);
-    }
-  };
+  // setInterval(findNewCall.bind(this), 3000);
+};
+
+Director.prototype.takeCall = function (call) {
+  console.log('take call director', call);
+  // var that = this;
+  if (call.level === 1) {
+    setTimeout(function () {
+      that.isBusy = true;
+    }, call.length);
+  }
 };
 
 //make this work!
 
-var managers = [];
-for(var i=0; i<10; i++){
-  managers.push(new Manager());
-}
-
-var respondents = [];
-for(var i=0; i<10; i++){
-  respondents.push(new Respondent());
-}
-
 var directors = [];
-for(var i=0; i<10; i++){
+for(var i=0; i<2; i++){
   directors.push(new Director());
 }
 
-setInterval(function(){
+var managers = [];
+for(var i=0; i<2; i++){
+  managers.push(new Manager(directors));
+}
 
-  var time = Math.random()*1000;
+var respondents = [];
+for(var i=0; i<4; i++){
+  respondents.push(new Respondent(managers));
+}
+
+
+setTimeout(function(){
+  var time = Math.random()*10000;
   var level = Math.floor(Math.random()*3) + 1;
-  var length = Math.random()*2000;
+  var length = Math.floor(Math.random()*2000);
+
+  console.log('making call', time, 1, length);
 
   setTimeout(function(){
-    dispatchCall(level,length);
+    dispatchCall(1,length);
   }, time);
 
-});
+}, 1000);
 
 //Questions that could be asked:
 
