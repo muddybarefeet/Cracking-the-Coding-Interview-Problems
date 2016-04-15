46c9aba1c13eb994637662404dcf862cbb7b3249
Anna Rogers <2muddybarefeet@gmail.com>
2016-04-15 07:09:23 -0700

    Solution to bubb;e sort

---
+++ toyProblems/sorting/bubbleSort.js
@@ -0,0 +1,33 @@
+//Take the numbers in situe and swap if the left one is greater than the right
+//once on pass has been made then see if it is totally sorted
+//else recurse
+
+var swap = function (array, currentIndex) {
+  //take the array and the current index and swap this with the index before
+  //return updated array
+  var currNum = array[currentIndex];
+  array[currentIndex] = array[currentIndex-1];
+  array[currentIndex-1] = currNum;
+
+  return array;
+};
+
+
+var bubbleSort = function (array) {
+  var hasBeenChanged;
+  //loop through the array starting at one and compare to the item before
+  for (var i = 1; i < array.length; i++) {
+    if ( array[i] < array[i-1] ) {
+      //then they need to be swapped
+      swap(array, i);
+      //mark a flag if anything gets changed so at the end know if sorted or not
+      hasBeenChanged = true;
+    }
+  }
+
+  return hasBeenChanged ? bubbleSort(array) : array;
+};
+
+var x = bubbleSort([4,3,7,2,1,9]);
+console.log(x);
+
