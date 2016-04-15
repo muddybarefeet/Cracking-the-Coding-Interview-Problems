4ffd7b68d9befa5f04794b3ad33c124f28394221
Anna Rogers <2muddybarefeet@gmail.com>
2016-04-15 07:08:16 -0700

    Initial commit for this question

---
+++ toyProblems/sorting/mergeSort.js
@@ -0,0 +1,9 @@
+//keep halving the array until it is split into individual numbers
+//then join the ones back together a pair at a time
+
+//function to split the array up
+var mergeSort = function () {
+
+};
+
+//function to merge the numbers back together
